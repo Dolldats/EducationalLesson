a510391ea0b67eeeaad12774b2ed0d22d6fb9f24
Dolldats <dollarsakintola@gmail.com>
2023-03-19 21:49:36 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EducationalLesson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
